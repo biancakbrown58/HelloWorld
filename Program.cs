34d80de0d261063c010188d4fa4eb86cb16578e4
Bianca Brown <biancakbrown58@gmail.com>
2020-02-10 15:52:43 -0500

    Did the thing

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+
+      Console.WriteLine("What is your name?");
+      var myName = Console.ReadLine();
+      Console.WriteLine("Hello, World, my name is " + myName);
+
+    }
+  }
+}
